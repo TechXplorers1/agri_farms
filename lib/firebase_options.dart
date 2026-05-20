134d87c8252881d90efcb55ba25c6394efc1d55e
chaveen-tx7 <bharath@techxplorers.in>
2026-05-20 19:22:31 +0530

    created the otp by the email for the web appa

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,13 +2,12 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
 class DefaultFirebaseOptions {
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAK4PoWvIMinNXZYNtHZ5w-l_dDyOYJBg0',
-    appId: '1:114650691864:web:70b5f47dfd001581c7866d',
-    messagingSenderId: '114650691864',
-    projectId: 'agrifarms-d7ccb',
-    authDomain: 'agrifarms-d7ccb.firebaseapp.com',
-    databaseURL: 'https://agrifarms-d7ccb-default-rtdb.firebaseio.com',
-    storageBucket: 'agrifarms-d7ccb.firebasestorage.app',
-    measurementId: 'G-YST45W800T',
+    apiKey: 'AIzaSyB6-nTwiYN91yAZhn0N3pp70auSNPJ45XU',
+    appId: '1:966639409242:web:c305e1d207344af67ae063',
+    messagingSenderId: '966639409242',
+    projectId: 'agrifarms-174f9',
+    authDomain: 'agrifarms-174f9.firebaseapp.com',
+    storageBucket: 'agrifarms-174f9.firebasestorage.app',
+    measurementId: 'G-C86KEQSTPM',
   );
 }
